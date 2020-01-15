75263162e329ac80d7d70401ff60bfd733dfc5c2
AMarinhoSN <amarinhosn@protonmail.com>
2020-01-15 14:10:04 +0100

    [ADD] Script to generate metadata for a lPDB copy

---
+++ mount_lpdb_mtdt.py
@@ -0,0 +1,30 @@
+import lPDB
+
+'''
+This script generates a dataframe with local PDB copy metadata.
+Currently:
+    1 - get pdb files location;
+    2 - associate each entry with different bc groups
+    3 - dssp out files
+    4 - xgeo files
+    5 - export metadata as a csv file
+    6 - save lPDB object state as a pickle file.
+'''
+# set lPDB source dir
+src_dir = '/home/users/amarinho/lPDB/'
+
+print('@ get entries info...')
+lPDB_obj = lPDB.lPDB(src_dir)
+print('@ get pdbid chains location...')
+lPDB_obj.add_chains_coordfls()
+print('@ add bc groups columns...')
+lPDB_obj.add_bcgrps_col()
+print('@ add dssp filepaths...')
+lPDB_obj.add_dssp_fls()
+print('@ add xgeo filepaths...')
+lPDB_obj.add_xgeo_fls()
+print('@ saving csv file...')
+lPDB_obj.export_to_csv()
+print('@ saving lPDB object state...')
+lPDB_obj.save_state()
+print(':: DONE ::')
